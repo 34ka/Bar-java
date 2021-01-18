8191ea0e095336435eb6956e50d049f1ed605a5c
34ka <60933524+34ka@users.noreply.github.com>
2021-01-18 22:40:24 +0600

    Revert "Реализована проверка и обеспечение выполнения предусловий"
    This reverts commit 0c6f21bcf0d1ca5d952b78615c2c83975f9e4e80.

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,16 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.model.ContactDate;
 
 public class ContactDeletionTests extends TestBase {
 
   @Test
   public void testContactDeletion() throws Exception {
-    if (! app.getContactHelper().isThereAContact()) {
-      app.getContactHelper().createContact(new ContactDate("Ivan", null, null, "test1"), true);
-    }
-    app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().selectContact();
     app.getContactHelper().deleteSelectedContacts();
     app.getContactHelper().closeAlertWindow();
