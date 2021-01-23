78962f806647ba11e1034f542becfe50cd53130a
34ka <60933524+34ka@users.noreply.github.com>
2021-01-23 20:17:51 +0600

    Вычисление максимального идентификатора (Создание контакта)

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactDate;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,20 +15,14 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() throws Exception {
     List<ContactDate> before = app.getContactHelper().getContactList();
     app.getContactHelper().openNewContactForm();
-    ContactDate contact = new ContactDate("Ivan", "Testovich", null, "test1");
+    ContactDate contact = new ContactDate("Ivans", "Testovich", null, "test1");
     app.getContactHelper().fillContactForm(contact, true);
     app.getContactHelper().submitContactForm();
     app.getNavigationHelper().gotoHomePage();
     List<ContactDate> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactDate g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((Comparator<ContactDate>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
