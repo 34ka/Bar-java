533915e3bdf3ff4fa4932fd1422a19625f66eecb
34ka <60933524+34ka@users.noreply.github.com>
2021-01-25 02:10:07 +0600

    исправление ссылки для перехода при создании группы

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -17,7 +17,7 @@ public class GroupHelper extends HelperBase {
     }
 
     public void returnToGroupPage() {
-        click(By.linkText("group page"));
+        click(By.linkText("groups"));
     }
 
     public void submitGroupCreation() {
